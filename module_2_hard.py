58e3b63ccf5e7e0b488308f41ebabdf4defd6cbd
AnzhelaAyd <angiemagomedova@yandex.com>
2024-09-01 22:16:32 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,16 @@
-a = int(input('chislo a= '))
-k = ''
-for i in range(1, a):
-    for j in range(i + 1, a):
-        if j <= i:
-            continue
-        if a % (i + j) == 0:
-            k = k + str(i) + str(j)
+def chislo(a):
+    k = ''
+    for i in range(1, a):
+        for j in range(i + 1, a):
+            if j <= i:
+                continue
+            if a % (i + j) == 0:
+                k = k + str(i) + str(j)
+    return k
 
-print(k)
+
+
+
+for a in range(3, 21):
+    b=chislo(a)
+    print(b)
